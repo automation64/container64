656b14418313bfae4a5a5a52118e9addf613e593
SerDigital64 <serdigital64@gmail.com>
2022-01-23 13:57:47 -0300

    refact: update lib

--- bin/bashlib64.bash
+++ bin/bashlib64.bash
@@ -5,7 +5,7 @@
 # Author: serdigital64 (https://github.com/serdigital64)
 # License: GPL-3.0-or-later (https://www.gnu.org/licenses/gpl-3.0.txt)
 # Repository: https://github.com/serdigital64/bashlib64
-# Version: 1.9.1
+# Version: 1.9.2
 #######################################
 
 [[ -n "$BL64_LIB_DEBUG" && "$BL64_LIB_DEBUG" == '1' ]] && set -x
@@ -903,7 +903,7 @@ bl64_os_set_alias
 bl64_sudo_set_alias
 
 if [[ "$BL64_LIB_CMD" = '1' ]]; then
-  "@"
+  "$@"
 else
   :
 fi
