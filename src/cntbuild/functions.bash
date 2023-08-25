4e475bda6738ee4670370c3cc6298e39d3581c64
SerDigital64 (Max) <serdigital64@gmail.com>
2023-08-25 17:20:08 -0300

    feat: add container img option (#9)
    * refact: upgrade bashlib64
    
    * fix: typo
    
    * refact: normalize name
    
    * feat: add cnt delete

--- src/cntbuild/functions.bash
+++ src/cntbuild/functions.bash
@@ -3,41 +3,48 @@
 #
 
 function cntbuild_list() {
-  bl64_dbg_app_show_function
+  bl64_dbg_app_show_function "$@"
   local context="$1"
+  local target=''
 
   bl64_check_parameter 'context' &&
     bl64_check_directory "${context}/${CNTBUILD_DOCKERFILE_SOURCES}" || return $?
 
+  target="${context}/${CNTBUILD_DOCKERFILE_SOURCES}"
+  bl64_msg_show_task "List container images available in the current context (${target})"
   # shellcheck disable=SC2164
-  cd "${context}/${CNTBUILD_DOCKERFILE_SOURCES}"
+  cd "$target"
 
-  printf '%s\n' *
+  # shellcheck disable=SC2086
+  bl64_fs_run_find . \
+    $BL64_FS_SET_FIND_TYPE_FILE \
+    $BL64_FS_SET_FIND_NAME 'Dockerfile' \
+    $BL64_FS_SET_FIND_PRINT
 }
 
 function cntbuild_build() {
-  bl64_dbg_app_show_function
+  bl64_dbg_app_show_function "$@"
   local container="$1"
-  local tag="$2"
-  local context="$3"
-  local labels_file="${context}/${CNTBUILD_DOCKERFILE_SOURCES}/${container}/${CNTBUILD_METADATA_FILE}"
+  local context="$2"
+  local tag="$3"
   local -a command_line
   local -a labels_version
   local -i labels_index=0
+  local labels_file=''
   local labels_record=''
 
   bl64_check_parameter 'container' &&
     bl64_check_parameter 'context' ||
     return $?
 
-  # Determine if standard labels are provided
+  labels_file="${context}/${CNTBUILD_DOCKERFILE_SOURCES}/${container}/${CNTBUILD_METADATA_FILE}"
   if [[ -f "$labels_file" ]]; then
-    # Load each label and add to the command line
+    bl64_dbg_app_show_info "get container labels (${labels_file})"
     IFS=$'\n'
     for labels_record in $(<"${labels_file}"); do
       unset IFS
       # shellcheck disable=SC2004
-      command_line[${labels_index}]="--label='$labels_record'"
+      command_line[${labels_index}]="--label=$labels_record"
       labels_index=$((labels_index + 1))
 
       # Extract the container version if provided
@@ -48,66 +55,100 @@ function cntbuild_build() {
       fi
 
     done
+    bl64_dbg_app_show_info "labels: ${command_line[*]}"
+  fi
+  if [[ -z "$tag" ]]; then
+    bl64_msg_show_error 'unable to determine container tag. Specify one either by using the labels file or the -e parameter'
+    return 1
   fi
 
-  bl64_cnt_build "$context" "${CNTBUILD_DOCKERFILE_SOURCES}/${container}/${CNTBUILD_DOCKERFILE_NAME}" "${container}:${tag}" "${command_line[@]}"
+  bl64_msg_show_task "Build container image (${container}:${tag})"
+  bl64_cnt_build \
+    "$context" \
+    "${CNTBUILD_DOCKERFILE_SOURCES}/${container}/${CNTBUILD_DOCKERFILE_NAME}" \
+    "$tag" \
+    "${command_line[@]}"
 }
 
 function cntbuild_open() {
-  bl64_dbg_app_show_function
+  bl64_dbg_app_show_function "$@"
+  local container="$1"
+  local context="$2"
+  local tag="$3"
+  local target=''
+
+  bl64_check_parameter 'container' &&
+    bl64_check_parameter 'context' ||
+    return $?
+
+  tag="$(cntbuild_get_version "$container" "$context" "$tag")" || return $?
+  target="${container}:${tag}"
+  bl64_msg_show_task "open shell in container image ($target})"
+  bl64_cnt_run_sh "$target"
+}
+
+function cntbuild_delete_github() {
   local container="$1"
   local tag="$2"
-  local context="$3"
-  # shellcheck disable=SC2178
-  local version=''
+  local api_path="/orgs/${CNTBUILD_REGISTRY_OWNER}/packages/container"
+  local api_query=''
+  local target="${container}${tag:+:}${tag}"
+
+  api_query="/$(bl64_api_url_encode "$target")"
+  bl64_msg_show_task "delete container image from GitHub registry ($target})"
+  bl64_vcs_github_run_api \
+    "$api_path" \
+    "$BL64_API_METHOD_DELETE" \
+    "$api_query" \
+    "${CNTBUILD_LOGIN_PASSWORD}"
+}
+
+function cntbuild_delete() {
+  bl64_dbg_app_show_function "$@"
+  local container="$1"
+  local tag="$2"
 
   bl64_check_parameter 'container' ||
     return $?
 
-  # shellcheck disable=SC2178
-  version="$(cntbuild_get_version "$container" "$context")" || return $?
-  # shellcheck disable=SC2128
-  [[ -n "$version" ]] && tag="$version"
-
-  bl64_cnt_run_sh "${container}:${tag}"
+  case "$CNTBUILD_REGISTRY" in
+  ghcr.io | ghcr.io/*)
+    cntbuild_delete_github "$container" "$tag"
+    ;;
+  *)
+    bl64_msg_show_error "Unsupported registry API (${CNTBUILD_REGISTRY})"
+    return 1
+    ;;
+  esac
 }
 
 function cntbuild_publish() {
-  bl64_dbg_app_show_function
+  bl64_dbg_app_show_function "$@"
   local container="$1"
-  local tag="$2"
-  local context="$3"
-  # shellcheck disable=SC2178
-  local version=''
+  local context="$2"
+  local tag="$3"
+  local target=''
 
   bl64_check_export 'CNTBUILD_LOGIN_USER' &&
     bl64_check_export 'CNTBUILD_LOGIN_PASSWORD' &&
-    bl64_check_export 'CNTBUILD_LOGIN_URL' ||
+    bl64_check_export 'CNTBUILD_REGISTRY' ||
     return $?
 
-  version="$(cntbuild_get_version "$container" "$context")" || return $?
-  # shellcheck disable=SC2128
-  [[ -n "$version" ]] && tag="$version"
-
-  bl64_cnt_login "$CNTBUILD_LOGIN_USER" "$CNTBUILD_LOGIN_PASSWORD" "$CNTBUILD_LOGIN_URL" &&
+  tag="$(cntbuild_get_version "$container" "$context" "$tag")" || return $?
+  target="${CNTBUILD_REGISTRY}/${container}:${tag}"
+  bl64_msg_show_task "Publish container image to registry (${CNTBUILD_REGISTRY}: ${target})" &&
+  bl64_cnt_login "$CNTBUILD_LOGIN_USER" "$CNTBUILD_LOGIN_PASSWORD" "$CNTBUILD_REGISTRY" &&
     bl64_cnt_tag "${container}:${tag}" "${container}:latest" &&
-    bl64_cnt_push "${container}:${tag}" "${CNTBUILD_LOGIN_URL}/${container}:${tag}" &&
-    bl64_cnt_push "${container}:latest" "${CNTBUILD_LOGIN_URL}/${container}:latest"
+    bl64_cnt_push "${container}:${tag}" "$target" &&
+    bl64_cnt_push "${container}:latest" "${CNTBUILD_REGISTRY}/${container}:latest"
 
-}
-
-function cntbuild_setup_globals() {
-  :
-}
-
-function cntbuild_check_requirements() {
-  [[ -z "$cntbuild_command" ]] && cntbuild_help && return 1
-  return 0
 }
 
 function cntbuild_get_version() {
+  bl64_dbg_app_show_function "@"
   local container="$1"
   local context="$2"
+  local tag="$3"
   local labels_file="${context}/${CNTBUILD_DOCKERFILE_SOURCES}/${container}/${CNTBUILD_METADATA_FILE}"
   local -a labels_version
   local -i labels_index=0
@@ -125,26 +166,78 @@ function cntbuild_get_version() {
         break
       fi
     done
+  elif [[ -n "$tag" ]]; then
+    echo "$tag"
+  else
+    bl64_msg_show_error 'unable to determine container tag. Specify one either by using the labels file or the -e parameter'
+    return 1
   fi
 
 }
 
+#######################################
+# Initialize environment
+#
+# Arguments:
+#   None
+# Outputs:
+#   Initializacion progress messages
+# Returns:
+#   0: initialization ok
+#   >: failed to initialize
+#######################################
+function cntbuild_initialize() {
+  bl64_dbg_app_show_function "@"
+  local command="$1"
+
+  bl64_check_parameter 'command' ||
+    { cntbuild_help && return 1; }
+
+  bl64_cnt_setup || return $?
+
+  # shellcheck disable=SC2249
+  case "$command" in
+  'cntbuild_build' | 'cntbuild_publish' | 'cntbuild_open' | 'cntbuild_delete')
+    bl64_check_export 'CNTBUILD_LOGIN_USER' &&
+      bl64_check_export 'CNTBUILD_LOGIN_PASSWORD' &&
+      bl64_check_export 'CNTBUILD_REGISTRY' &&
+      bl64_check_export 'CNTBUILD_REGISTRY_OWNER' ||
+      return $?
+    ;;
+  esac
+
+  return 0
+}
+
+#######################################
+# Show script usage description
+#
+# Arguments:
+#   None
+# Outputs:
+#   Command line format and description
+# Returns:
+#   0
+#######################################
 function cntbuild_help() {
 
   bl64_msg_show_usage \
-    '<-b|-u|-l|-n> [-c Container] [-e Tag] [-o Context] [-h]' \
+    '<-b|-u|-l|-n|-x> [-c Container] [-e Tag] [-o Context] [-V Verbose] [-D Debug] [-h]' \
     'Build containers in dev environment' \
     '
   -b          : Build container
   -u          : Publish container to public registry
   -l          : List container sources
   -n          : Open local container
+  -x          : Delete container from registry. Supported registries: GitHub
     ' '
   -h          : Show help
     ' "
   -c Container: Container name: Format: base directory where the Dockerfile is. Required for -b, -l, -n
   -e Tag      : Container tag. Format: tag. Default: 0.1.0
   -o Context  : Build context. Format: full path. Default: PWD/src
+  -V Verbose  : Set verbosity level. Format: one of BL64_MSG_VERBOSE_*
+  -D Debug    : Enable debugging mode. Format: one of BL64_DBG_TARGET_*
   "
 
 }
--- src/cntbuild/main.bash
+++ src/cntbuild/main.bash
@@ -3,51 +3,43 @@
 #
 
 declare cntbuild_status=1
-declare cntbuild_command_tag=''
-declare cntbuild_command=''
+declare cntbuild_debug="$BL64_DBG_TARGET_NONE"
+declare cntbuild_verbose="$BL64_MSG_VERBOSE_APP"
 declare cntbuild_option=''
+declare cntbuild_command="$BL64_VAR_NULL"
 declare cntbuild_container=''
-declare cntbuild_tag='0.1.0'
+declare cntbuild_tag=''
 declare cntbuild_context=''
 cntbuild_context="$(pwd)/src"
 
 (($# == 0)) && cntbuild_help && exit 1
-while getopts ':bulnc:e:o:h' cntbuild_option; do
+while getopts ':bulnxc:e:o:V:D:h' cntbuild_option; do
   case "$cntbuild_option" in
-  b)
-    cntbuild_command='cntbuild_build'
-    cntbuild_command_tag='build container'
-    ;;
-  u)
-    cntbuild_command='cntbuild_publish'
-    cntbuild_command_tag='publish images to external registry'
-    ;;
-  l)
-    cntbuild_command='cntbuild_list'
-    cntbuild_command_tag='list container sources'
-    ;;
-  n)
-    cntbuild_command='cntbuild_open'
-    cntbuild_command_tag='open local container image'
-    ;;
+  b) cntbuild_command='cntbuild_build' ;;
+  u) cntbuild_command='cntbuild_publish' ;;
+  l) cntbuild_command='cntbuild_list' ;;
+  n) cntbuild_command='cntbuild_open' ;;
+  x) cntbuild_command='cntbuild_delete' ;;
   c) cntbuild_container="$OPTARG" ;;
   e) cntbuild_tag="$OPTARG" ;;
   o) cntbuild_context="$OPTARG" ;;
+  V) cntbuild_verbose="$OPTARG" ;;
+  D) cntbuild_debug="$OPTARG" ;;
   h) cntbuild_help && exit 0 ;;
   *) cntbuild_help && exit 1 ;;
   esac
 done
-cntbuild_setup_globals &&
-  cntbuild_check_requirements &&
-  bl64_cnt_setup || exit
+bl64_dbg_set_level "$cntbuild_debug" && bl64_msg_set_level "$cntbuild_verbose" || exit $?
+cntbuild_initialize "$cntbuild_command" || exit $?
 
-bl64_msg_show_batch_start "$cntbuild_command_tag"
+bl64_msg_show_batch_start "$cntbuild_command"
 case "$cntbuild_command" in
-'cntbuild_build' | 'cntbuild_publish' | 'cntbuild_open') "$cntbuild_command" "$cntbuild_container" "$cntbuild_tag" "$cntbuild_context" ;;
+'cntbuild_build' | 'cntbuild_publish' | 'cntbuild_open') "$cntbuild_command" "$cntbuild_container" "$cntbuild_context" "$cntbuild_tag";;
 'cntbuild_list') "$cntbuild_command" "$cntbuild_context" ;;
-*) bl64_check_show_undefined "$cntbuild_command" ;;
+'cntbuild_delete') "$cntbuild_command" "$cntbuild_container" "$cntbuild_tag" ;;
+*) bl64_check_alert_parameter_invalid "$cntbuild_command" ;;
 esac
 cntbuild_status=$?
 
-bl64_msg_show_batch_finish $cntbuild_status "$cntbuild_command_tag"
+bl64_msg_show_batch_finish $cntbuild_status "$cntbuild_command"
 exit $cntbuild_status
