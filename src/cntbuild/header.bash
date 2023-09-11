4b5ad5123fad95d1220b0e63153f177e9d175f1a
serdigital64 <serdigital64@gmail.com>
2023-09-11 13:12:47 -0300

    refact: replace license

--- src/cntbuild/header.bash
+++ src/cntbuild/header.bash
@@ -1,11 +1,27 @@
 #!/usr/bin/env bash
 #######################################
-# Build containers
+# CntBuild
+#
+# Container images build tool
 #
 # Author: serdigital64 (https://github.com/serdigital64)
-# License: GPL-3.0-or-later (https://www.gnu.org/licenses/gpl-3.0.txt)
 # Repository: https://github.com/automation64/container64
-# Version: 2.0.0
+# Version: 3.0.0
+#
+#######################################
+# Copyright [X_COPYRIGHT_YEAR_X] [X_COPYRIGHT_OWNER_X]
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #######################################
 
 #
