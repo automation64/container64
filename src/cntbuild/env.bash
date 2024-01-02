fb131a238dd0b8ffd450ee75a18b5e2ff2ca5a32
serdigital64 <serdigital64@gmail.com>
2024-01-02 15:38:35 +0000

    feat: add cosign

--- src/cntbuild/env.bash
+++ src/cntbuild/env.bash
@@ -7,8 +7,11 @@
 #
 
 # Registry credentials
-export CNTBUILD_REGISTRY
-export CNTBUILD_REGISTRY_OWNER
+export CNTBUILD_REGISTRY="${CNTBUILD_REGISTRY:-}"
+export CNTBUILD_REGISTRY_OWNER="${CNTBUILD_REGISTRY_OWNER:-}"
+
+# Cosign binary
+export CNTBUILD_COSIGN_BIN="${CNTBUILD_COSIGN_BIN:-/usr/local/bin/cosign}"
 
 # Metadata filename
 export CNTBUILD_METADATA_FILE='Metadata.label'
