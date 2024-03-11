6e8e3a10a7f4a0f93552966d2518bc3d9eaaea3d
serdigital64 <serdigital64@gmail.com>
2024-03-11 15:58:40 +0000

    doc: add skeletons

---
+++ docs/snippets/helper-setup/configure_os.bash
@@ -0,0 +1,9 @@
+# Snippet: 1.0.0
+# X_CODE_PLACEHOLDER_3_X
+
+function cnt64_setup_configure_os() {
+  bl64_dbg_app_show_function
+}
+
+# X_CODE_PLACEHOLDER_1_X
+  cnt64_setup_configure_os
---
+++ docs/snippets/helper-setup/configure_users.bash
@@ -0,0 +1,15 @@
+# Snippet: 1.0.0
+# X_CODE_PLACEHOLDER_4_X
+export CNT64_USER
+
+# X_CODE_PLACEHOLDER_3_X
+
+function cnt64_setup_configure_users() {
+  bl64_dbg_app_show_function
+}
+
+# X_CODE_PLACEHOLDER_2_X
+  bl64_check_export 'CNT64_USER' &&
+
+# X_CODE_PLACEHOLDER_1_X
+  cnt64_setup_configure_users
